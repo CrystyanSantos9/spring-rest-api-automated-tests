f6f04b9215e3de7542689302d035eed1661c0fcc
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-01-25 08:26:51 -0300

    feat/AUTOMATEDTESTS - projeto inicial

---
+++ src/main/java/dev/cryss/apiautomatedtests/ApiAutomatedTestsApplication.java
@@ -0,0 +1,13 @@
+package dev.cryss.apiautomatedtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAutomatedTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAutomatedTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cryss/apiautomatedtests/ApiAutomatedTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cryss.apiautomatedtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAutomatedTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
