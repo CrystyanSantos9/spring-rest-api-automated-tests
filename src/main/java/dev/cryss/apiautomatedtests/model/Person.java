efe67f1575967592d55593cb671463f7fd594145
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-01-26 20:17:14 -0300

    feat/AUTOMATEDTESTS - consertando versão do maven

--- src/main/java/dev/cryss/apiautomatedtests/model/Person.java
+++ src/main/java/dev/cryss/apiautomatedtests/model/Person.java
@@ -1,5 +1,11 @@
 package dev.cryss.apiautomatedtests.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,14 +21,26 @@ import java.io.Serializable;
 @Getter
 @Setter
 @Builder
+@Entity
+@Table(name="person")
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
 }
